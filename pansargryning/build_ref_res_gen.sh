63fce0d0e2acfdf70d37403ed49f48f015ac158f
Michal Malý <madcatxster@devoid-pointer.net>
2018-11-28 16:32:45 +0100

    Begin removing placeholder names

--- pansargryning/build_ref_res_gen.sh
+++ pansargryning/build_ref_res_gen.sh
@@ -4,7 +4,7 @@ source ../ref_tool/ref_tool_glob.sh
 clang -c ../ref_tool/jsonloader/constituents_json_ldr.c \
 	-I${LIBJANSSON_INCLUDE}
 clang++ -std=c++98 -Wall -Wextra -pedantic -g -O0 \
-	pansargryning_resgen.cpp ../ref_tool/json_input_processor.cpp ../ref_tool/jsonloader/inputreader.cpp \
+	reference_results_generator.cpp ../ref_tool/json_input_processor.cpp ../ref_tool/jsonloader/inputreader.cpp \
 	constituents_json_ldr.o ${LIBJANSSON_BIN} \
 	-o pgn_resgen \
 	-I${LEMNG_INCLUDE} \
